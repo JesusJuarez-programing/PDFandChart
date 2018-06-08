c1c12df8a91028f3424b6698cded5afe5ec9ddc0
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-08 10:45:48 -0500

    Graficos semi dinamicos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,20 +10,42 @@ export class HomePage {
   valores: number [] = [500, 600, 700];
 
   paises: string [] = ['México', 'USA', 'España', 'Cataluña'];
-  valor: number [] = [300, 400, 50, 2];
+  valor: number [] = [300, 400, 50, 20];
 
   empresas: string [] = ['SpaceX', 'Tesla'];
   val: number [] = [10, 10];
 
-  constructor(public navCtrl: NavController) {
+  doughnutChartLabels: string [];
+  doughnutChartData: number [];
+  public doughnutChartType: string = 'pie';
+  conta : number = 0;
 
+  constructor(public navCtrl: NavController) {
+    this.contar();
+  }
+
+  contar(){
+    this.conta ++;
+    this.doughnutChartLabels = [];
+      this.doughnutChartData =[];
+    if (this.conta == 1){
+      this.doughnutChartLabels = this.nombres;
+      this.doughnutChartData = this.valores;
+    }
+    if(this.conta == 2){
+      this.doughnutChartLabels = this.paises;
+      this.doughnutChartData = this.valor;
+    }
+    if(this.conta == 3){
+      this.doughnutChartLabels = this.empresas;
+    this.doughnutChartData = this.val;
+    }
   }
 
 
 // Estatico
-public doughnutChartLabels:string[] = this.nombres;
-public doughnutChartData:number[] = this.valores;
-public doughnutChartType:string = 'pie';
+
+
 
 
 // Dinamico
