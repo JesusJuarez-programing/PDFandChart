9c97b239709744d60dfb902d86d03118312c540e
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-12 10:52:27 -0500

    Siguiendo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  preguntas: string[] = ['', 'Pregunta 1', 'Pregunta 2', 'Pregunta 3'];
   nombres: string [] = ['Polo', 'Julio', 'Verne'];
   valores: number [] = [500, 600, 700];
 
@@ -19,6 +20,7 @@ export class HomePage {
   doughnutChartData: number [];
   public doughnutChartType: string = 'pie';
   conta : number = 0;
+  pregunta: string;
 
   constructor(public navCtrl: NavController) {
     this.contar();
@@ -29,17 +31,21 @@ export class HomePage {
     this.doughnutChartLabels = [];
       this.doughnutChartData =[];
     if (this.conta == 1){
+      this.pregunta = this.preguntas[this.conta];
+      console.log(this.pregunta);
       this.doughnutChartLabels = [];
       this.doughnutChartLabels = this.paises;
       
       this.doughnutChartData = this.valor;
     }
     if(this.conta == 2){
+      this.pregunta = this.preguntas[this.conta];
       this.doughnutChartLabels = [];
       this.doughnutChartLabels = this.nombres;
       this.doughnutChartData = this.valores;
     }
     if(this.conta == 3){
+      this.pregunta = this.preguntas[this.conta];
       this.doughnutChartLabels = [];
       this.doughnutChartLabels = this.empresas;
       this.doughnutChartData = this.val;
@@ -51,17 +57,20 @@ export class HomePage {
     this.doughnutChartLabels = [];
       this.doughnutChartData =[];
     if (this.conta == 1){
+      this.pregunta = this.preguntas[this.conta];
       this.doughnutChartLabels = [];
       this.doughnutChartLabels = this.paises;
       
       this.doughnutChartData = this.valor;
     }
     if(this.conta == 2){
+      this.pregunta = this.preguntas[this.conta];
       this.doughnutChartLabels = [];
       this.doughnutChartLabels = this.nombres;
       this.doughnutChartData = this.valores;
     }
     if(this.conta == 3){
+      this.pregunta = this.preguntas[this.conta];
       this.doughnutChartLabels = [];
       this.doughnutChartLabels = this.empresas;
       this.doughnutChartData = this.val;
